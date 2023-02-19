7749ee76eab1ee2705f499ec9d255f5777f1811e
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-19 23:04:20 +0300

    No zeros for heros

---
+++ ДЗ №1/task_5.py
@@ -0,0 +1,15 @@
+def no_boring_zeros(n):
+    lst = list(str(n))
+
+    for el in lst[::-1]:
+        if len(lst) == 1:
+            break
+        elif el == '0':
+            lst.pop(-1)
+        else:
+            break
+
+    return int(''.join(lst))
+
+if __name__ == '__main__':
+    print(no_boring_zeros(21000))
