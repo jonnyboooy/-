266993cc5f4cc6db11548327709aeca29f7991c9
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-20 01:14:39 +0300

    Katastrophe!

---
+++ ДЗ №1/task_8.py
@@ -0,0 +1,6 @@
+import math
+def strong_enough( earthquake, age ):
+    return 'Safe!' if sum(earthquake[0])*sum(earthquake[1])*sum(earthquake[2]) < 1000 * math.e**(-age/100) else 'Needs Reinforcement!'
+
+if __name__ == '__main__':
+    print(strong_enough([[5,8,7],[3,3,1],[4,1,2]], 2))
