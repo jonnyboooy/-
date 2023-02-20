90d368d9ea10d3a737276598abc3d8d96943dc46
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-20 16:47:27 +0300

    Katastrophe!

--- ДЗ №1/task_8.py
+++ ДЗ №1/task_8.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com//kata/55a3cb91d1c9ecaa2900001b
 import math
 def strong_enough( earthquake, age ):
     return 'Safe!' if sum(earthquake[0])*sum(earthquake[1])*sum(earthquake[2]) < 1000 * math.e**(-age/100) else 'Needs Reinforcement!'
