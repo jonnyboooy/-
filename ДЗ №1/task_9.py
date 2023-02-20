fdb63116bc637d230a123bc881c55c416d995d16
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-20 16:48:12 +0300

    Jumping Number (Special Numbers Series #4)

--- ДЗ №1/task_9.py
+++ ДЗ №1/task_9.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com//kata/5a54e796b3bfa8932c0000ed
 def jumping_number(number):
     for i in range(len(str(number))-1):
         if abs(int(str(number)[i])-int(str(number)[i+1])) != 1: return 'Not!!'
