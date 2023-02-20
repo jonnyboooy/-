36eaf8b3612b762928e93c783b817f5687056823
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-20 16:48:58 +0300

    Alternate Square Sum

--- ДЗ №1/task_10.py
+++ ДЗ №1/task_10.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com//kata/559d7951ce5e0da654000073
 def alternate_sq_sum(arr):
     return (sum(pow(arr[i], 2) if i%2 != 0 else 0 for i in range(len(arr))) + sum(arr[i] if i%2 == 0 else 0 for i in range(len(arr))))
 
