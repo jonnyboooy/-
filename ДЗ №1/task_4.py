95a700a40c04ca74a1eab47cf4888ed8d236eb04
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-02-20 16:43:15 +0300

    Calculate BMI

--- ДЗ №1/task_4.py
+++ ДЗ №1/task_4.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com//kata/57a429e253ba3381850000fb
 def bmi(weight, height):
     if weight/pow(height, 2) <= 18.5: return 'Underweight'
     elif weight/pow(height, 2) <= 25: return 'Normal'
