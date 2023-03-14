5ea59c551380e362a8008ee0d2d7e38b7173dc8a
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-03-14 11:13:18 +0300

    Reverse String

--- ДЗ №3/task_7.py
+++ ДЗ №3/task_7.py
@@ -1,7 +1,12 @@
 # https://leetcode.com/problems/reverse-string/
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s)//2):
-            buff = s[i]
-            s[i] = s[len(s)-i-1]
-            s[len(s)-i-1] = buff
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+            buff = s[l]
+            s[l] = s[r]
+            s[r] = buff
+            l += 1
+            r -= 1
